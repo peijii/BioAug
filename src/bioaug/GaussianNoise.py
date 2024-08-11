b52f43352b4b0682ebcdb0bd6c2e473c5c39c43f
Peiji <c2142009@edu.cc.uec.ac.jp>
2024-08-11 22:56:09 +0900

    add permutation

--- src/bioaug/GaussianNoise.py
+++ src/bioaug/GaussianNoise.py
@@ -16,35 +16,35 @@ class GaussianNoise(object):
         self.seed = seed
 
     def __call__(self, signal):
-        """signal: [channel, length]"""
+        """signal: [length, channel]"""
         if np.random.uniform(0, 1) < self.p:
             signal_ = np.array(signal).copy()
             np.random.seed(self.seed)
-            channels = signal_.shape[0]
-            length = signal_.shape[1]
+            length = signal_.shape[0]
+            channels = signal_.shape[1]
             for i in range(channels):
                 noise = np.random.randn(length)
                 noise = noise - np.mean(noise)
-                signal_power = (1 / length) * np.sum(np.power(signal_[i, :], 2))
+                signal_power = (1 / length) * np.sum(np.power(signal_[:, i], 2))
                 noise_variance = signal_power / np.power(10, (self.SNR / 10))
                 noise = (np.sqrt(noise_variance) / np.std(noise)) * noise
-                signal_[i, :] = signal_[i, :] + noise
+                signal_[:, i] = signal_[:, i] + noise
             return signal_
         return signal
 
 
 if __name__ == '__main__':
-    data = np.random.normal(loc=1, scale=1, size=(6, 500))
-    gn = GaussianNoise(p=1.0, SNR=5)
+    data = np.random.normal(loc=1, scale=1, size=(500, 6))
+    gn = GaussianNoise(p=1.0, SNR=15)
     aug_data = gn(data)
 
     raw_fig = plt.figure(figsize=(10, 10))
     for plt_index in range(1, 7):
         ax = raw_fig.add_subplot(3, 2, plt_index)
-        ax.plot(list(range(500)), data[plt_index-1])
+        ax.plot(list(range(500)), data[:, plt_index-1])
 
     aug_fig = plt.figure(figsize=(10, 10))
     for plt_index in range(1, 7):
         ax = aug_fig.add_subplot(3, 2, plt_index)
-        ax.plot(list(range(500)), aug_data[plt_index-1], color='r')
+        ax.plot(list(range(500)), aug_data[:, plt_index-1], color='r')
     plt.show()
--- src/bioaug/Permutation.py
+++ src/bioaug/Permutation.py
@@ -1,1 +1,65 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Permutation(object):
+    """Permutate the input time-series data randomly with a given probability.
+    
+    Args:
+        p             (float) : probability of applying permutation to the input signal.
+        nPerm         (int)   : sd of the scale value
+        minSegLength  (int)   : Length of the input data.
+    """
+
+    def __init__(self, p=0.5, nPerm=4, minSegLength=10):
+        self.p = p
+        self.nPerm = nPerm
+        self.minSegLength = minSegLength
+
+    def __call__(self, signal):
+        """signal: [length, channel]"""
+        length = signal.shape[0]
+        channels = signal.shape[1]
+        if np.random.uniform(0, 1) < self.p:
+            if channels == 1:
+                signal_ = np.squeeze(np.zeros((length, channels)))
+            else:
+                signal_ = np.zeros((length, channels))
+            idx = np.random.permutation(self.nPerm)
+            flag = True
+            while flag == True:
+                segs = np.zeros(self.nPerm + 1, dtype=int)
+                segs[1:-1] = np.sort(
+                    np.random.randint(self.minSegLength, length - self.minSegLength, self.nPerm - 1))
+                segs[-1] = length
+                if np.min(segs[1:] - segs[0:-1]) > self.minSegLength:
+                    flag = False
+
+            pp = 0
+            for i in range(self.nPerm):
+                if channels == 1:
+                    signal_temp = signal[segs[idx[i]]:segs[idx[i] + 1]]
+                    signal_[pp:pp + len(signal_temp)] = signal_temp
+                else:
+                    signal_temp = signal[segs[idx[i]]:segs[idx[i] + 1], :]
+                    signal_[pp:pp + len(signal_temp), :] = signal_temp
+                pp += len(signal_temp)
+            return signal_
+        return signal
+
+
+if __name__ == '__main__':
+    data = np.random.normal(loc=1, scale=1, size=(500, 6))
+    pe = Permutation(p=1.0, nPerm=2, minSegLength=10)
+    aug_data = pe(data)
+
+    raw_fig = plt.figure(figsize=(10, 10))
+    for plt_index in range(1, 7):
+        ax = raw_fig.add_subplot(3, 2, plt_index)
+        ax.plot(list(range(500)), data[:, plt_index-1])
+
+    aug_fig = plt.figure(figsize=(10, 10))
+    for plt_index in range(1, 7):
+        ax = aug_fig.add_subplot(3, 2, plt_index)
+        ax.plot(list(range(500)), aug_data[:, plt_index-1], color='r')
+    plt.show()
